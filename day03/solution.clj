966860449e1c995dc01b6dffdca437497822e2c3
Piotr Kaznowski <piotr@kazno.dev>
2023-01-09 22:59:53 +0100

    Day 3

---
+++ day03/solution.clj
@@ -0,0 +1,12 @@
+(ns day03.solution
+  (:require [input :refer [f->nums]]))
+
+(defn transpose [m] (apply mapv vector m))
+
+(defn -main [day]
+  (let [rows (->> day f->nums (partition 3))
+        columns (->> rows transpose flatten (partition 3))
+        sides (fn [[a b c]] [[a b c] [a c b] [b c a]])
+        triangle? #(every? (fn [[a b c]] (> (+ a b) c)) (sides %))
+        solve #(->> % (filter triangle?) count)]
+    {:part1 (solve rows) :part2 (solve columns)}))

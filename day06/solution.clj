5463c5a80d1f1d7651d19b335183cd3f11480ea2
Piotr Kaznowski <piotr@kazno.dev>
2023-01-11 22:34:21 +0100

    Day 6

---
+++ day06/solution.clj
@@ -0,0 +1,29 @@
+(ns day06.solution
+  (:require [input :refer [f->lines lines]]))
+
+(def r->c #(apply mapv vector %))
+(def common #(->> % frequencies (sort-by val) ((juxt first last)) (map first)))
+(defn solve [it] (->> it r->c (map common) r->c (map #(apply str %))))
+
+(defn -main [day] (->> day f->lines solve (zipmap [:part2 :part1])))
+
+
+(comment
+  (let [test-input "eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar"]
+    (->> test-input lines solve (= '("advent" "easter"))))
+  )

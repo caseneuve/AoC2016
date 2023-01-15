6c75fbf0ab2f6c4682cf3d00c1db40e838061311
Piotr Kaznowski <piotr@kazno.dev>
2023-01-15 16:24:50 +0100

    Refactor day 10

--- Clojure/day10.clj
+++ Clojure/day10.clj
@@ -14,14 +14,15 @@
      (fn [m [bot low high]]
        (let [[lo hi :as v] (sort (m bot)), out (vals (select-keys m ["output0" "output1" "output2"]))]
          (cond
-           (and (= part 1) (= v (m "comp")))  (reduced bot)
+           (and (= part 1) (= v (m :compared)))  (reduced bot)
            (and (= part 2) (= (count out) 3)) (reduced (->> out flatten (apply *)))
            (not= (count v) 2) m
            :else (-> m (dissoc bot) (update low conj lo) (update high conj hi)))))
      chips (cycle instructions)))
 
 (defn -main [day]
-  (let [[ch ii] (->> day f->lines parse), solve (partial inspect (assoc ch "comp" [17 61]) ii)]
+  (let [[chips instructions] (->> day f->lines parse)
+        solve (partial inspect (assoc chips :compared [17 61]) instructions)]
     {:part1 (solve 1) :part2 (solve 2)}))
 
 
@@ -33,6 +34,5 @@ bot 1 gives low to output 1 and high to bot 0
 bot 0 gives low to output 2 and high to output 0
 value 2 goes to bot 2"
         [ch ii] (->> test-input lines parse), solve (partial inspect (assoc ch "comp" [2 5]) ii)]
-    {:1 (= "bot2" (solve 1)) :2 (= (* 5 2 3) (solve 2))}
-    )
+    {:1 (= "bot2" (solve 1)) :2 (= (* 5 2 3) (solve 2))})
   )

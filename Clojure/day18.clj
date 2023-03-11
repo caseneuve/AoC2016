e973af4560a895fa816f28cfba42161800ea3c45
Piotr Kaznowski <piotr@kazno.dev>
2023-03-11 20:24:50 +0100

    Day 18 simplified

--- Clojure/day18.clj
+++ Clojure/day18.clj
@@ -9,9 +9,10 @@
                (fn [n lcr] (conj n (if (contains? traps lcr) \^ \.)))
                [] (partition 3 1 (conj (vec (concat [\.] r)) \.))))]
     (loop [row input, result {:part1 nil :part2 0}, n 0]
-      ;; (when (= 0 (mod n 10000)) (prn n (result :part2)))
-      (let [s (count (filter #(= % \.) row))]
-        (cond
-          (= n 400000) result
-          (= n 40) (recur (nxt row) (-> result (assoc :part1 (result :part2)) (update :part2 + s)) (inc n))
-          :else    (recur (nxt row) (update result :part2 + s) (inc n)))))))
+      (when (= 0 (mod n 10000)) (prn n (result :part2)))
+      (if (= n 400000) result
+          (recur (nxt row)
+                 (cond-> result
+                   (= n 40) (assoc :part1 (result :part2))
+                   :always (update :part2 + (count (filter #(= % \.) row))))
+                 (inc n))))))
